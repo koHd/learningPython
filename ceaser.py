1f9161cf8f00c35f3b80c410af9e72813a0c9048
Shane Farrell <shane.farrell@yahoo.ie>
2015-11-04 15:47:27 +0000

    Broke the algorithm to shift an individual character out into it's own function.

--- ceaser.py
+++ ceaser.py
@@ -5,17 +5,21 @@ punctuationSymbols = set(string.punctuation)
 plaintext = input("Input the message: ")
 shift = int(input("Choose a shift value: "))
 
-def encrypt(plaintext):
+def shiftCharacter(character, shift):
+	assert (character.isalpha()), "The symbol '" + character + "' is not in my alphabet. Cannot encrypt. "
+	newCharacterIndex = (alphabet.index(character) + shift) % len(alphabet)
+	newCharacter = alphabet[newCharacterIndex]
+	return newCharacter
+
+def encrypt(plaintext, shift):
 	ciphertext = str()
 	for character in plaintext.lower():
 		if character.isspace() or character in punctuationSymbols:
 			ciphertext += character
 		else:
-			assert (character.isalpha()), "The symbol '" + character + "' is not in my alphabet. Cannot encrypt. "
-			shiftedCharacterIndex = (alphabet.index(character) + shift) % len(alphabet)
-			ciphertext += alphabet[shiftedCharacterIndex]
+			ciphertext += shiftCharacter(character, shift)
 	return ciphertext
 
-ciphertext = encrypt(plaintext)
+ciphertext = encrypt(plaintext, shift)
 
 print(ciphertext)
