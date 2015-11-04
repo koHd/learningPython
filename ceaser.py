0565b154895c7627c07c4b8bfb50317761c48306
Shane Farrell <shane.farrell@yahoo.ie>
2015-11-04 14:30:45 +0000

    Converted ecryption algorithm to a callable function that takes a plaintext as an argument and returns the ciphertext.

--- ceaser.py
+++ ceaser.py
@@ -1,13 +1,17 @@
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 plaintext = input("Input the message: ")
 shift = int(input("Choose a shift value: "))
-ciphertext = str()
 
-for character in plaintext.lower():
-	if character == ' ':
-		ciphertext += ' '
-	else:
-		newCharacterIndex = (alphabet.index(character) + shift) % len(alphabet)
-		ciphertext += alphabet[newCharacterIndex]
+def encrypt(plaintext):
+	ciphertext = str()
+	for character in plaintext.lower():
+		if character == ' ':
+			ciphertext += ' '
+		else:
+			shiftedCharacterIndex = (alphabet.index(character) + shift) % len(alphabet)
+			ciphertext += alphabet[shiftedCharacterIndex]
+	return ciphertext
+
+ciphertext = encrypt(plaintext)
 
 print(ciphertext)
