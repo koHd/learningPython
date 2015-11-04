9c69b7830f0755f06be873554055e0d834789024
kohd <shane.farrell@yahoo.ie>
2015-11-04 12:20:33 +0000

    Testing cli arguments and discovered python 2 print statement v python 3 print() function differences.

---
+++ cliArguments.py
@@ -0,0 +1,7 @@
+import sys
+
+print ('Number of arguments:', len(sys.argv), 'arguments.')
+print ('Argument list:', str(sys.argv))
+
+# note: python 3 replaced python 2's print statement with the print() function
+# print 'string' will not work in python 3!
