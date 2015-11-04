7554035aeb2960d1970fe68bbc24f9915437b426
Shane Farrell <shane.farrell@yahoo.ie>
2015-11-04 15:35:00 +0000

    Encryption can now handle punctuation symbols as well as blank spaces better. The ciphertext will simply maintain such symbols/blanks as they are.

--- ceaser.py
+++ ceaser.py
@@ -1,14 +1,17 @@
+import string
+
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
+punctuationSymbols = set(string.punctuation)
 plaintext = input("Input the message: ")
 shift = int(input("Choose a shift value: "))
 
 def encrypt(plaintext):
 	ciphertext = str()
 	for character in plaintext.lower():
-		if character == ' ':
-			ciphertext += ' '
+		if character.isspace() or character in punctuationSymbols:
+			ciphertext += character
 		else:
-			assert (character.isalpha()), "The symbol " + character + " is not in my alphabet. Cannot encrypt. "
+			assert (character.isalpha()), "The symbol '" + character + "' is not in my alphabet. Cannot encrypt. "
 			shiftedCharacterIndex = (alphabet.index(character) + shift) % len(alphabet)
 			ciphertext += alphabet[shiftedCharacterIndex]
 	return ciphertext
