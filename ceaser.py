8e4252c5920acb703a049c32664a0cc2429459e6
Shane Farrell <shane.farrell@yahoo.ie>
2015-11-10 21:49:09 +0000

    Ciphertext output file name now taken as program argument.

--- ceaser.py
+++ ceaser.py
@@ -7,8 +7,10 @@ shift = int(input("Choose a shift value: "))
 
 parser = argparse.ArgumentParser(description='Shift encrypt a plaintext file.')
 parser.add_argument('-p', '--plaintext', help='Input plaintext file name', required=True)
+parser.add_argument('-c', '--ciphertext', help='Name for ciphertext file', required=True)
 args = parser.parse_args()
 plaintextFile = open(args.plaintext)
+ciphertextFile = open(args.ciphertext, "w")
 
 def shiftCharacter(character, shift):
 	assert (character.isalpha()), "The symbol '" + character + "' is not in my alphabet. Cannot encrypt. "
@@ -25,5 +27,4 @@ def encrypt(plaintext, shift):
 			ciphertext += shiftCharacter(character, shift)
 	return ciphertext
 
-ciphertextFile = open("ciphertext.txt", "w")
 ciphertextFile.write(encrypt(plaintextFile.read(), shift))
